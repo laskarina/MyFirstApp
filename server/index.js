2c02307b59c0817909cdbd51e00b598ae5fe09d4
laskarina <a.toki0117@gmail.com>
2020-11-17 15:20:54 +0900

    Heroku用セットアップ

--- server/index.js
+++ server/index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const config = require('./config/dev');
+const config = require('./config');
 const FakeDb = require('./fake-db')
 
 const productRoutes = require('./routes/products');
+const path = require('path')
 
 //index.js読み込み時、fake-dbよりデータをclear&insert
 mongoose.connect(config.DB_URI, {
     useNewUrlParser: true
 }).then(
     () => {
-        const fakeDb = new FakeDb()
-        fakeDb.initDb()
+
+        //DB初期化はテスト環境且つ、実施したい時のみコメントアウトを外す事
+        if(process.env.NODE_ENV !== 'production'){
+            const fakeDb = new FakeDb()
+            //fakeDb.initDb()
+        }
     }
 )
 
@@ -19,6 +24,16 @@ const app = express()
 
 ///api/v1/productsがリクエストされた場合、productRoutesを実行
 app.use('/api/v1/products', productRoutes)
+
+//本番環境でBuildする時のみdist配下を読み込ませる
+if(process.env.NODE_ENV === 'production'){
+    const appPath = path.join( __dirname,'..', 'dist', 'my-first-app')
+    app.use(express.static(appPath))
+    app.get("*", function(req, res){
+        res.sendFile(path.resolve(appPath, "index.html"))
+    })
+}
+
 
 const PORT = process.env.PORT || '3001'
 
